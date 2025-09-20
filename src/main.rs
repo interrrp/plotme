05b38caa100f677dba47b8018ef5438f4478e565
interrrp <interrrp@proton.me>
2025-09-20 21:31:27 +0800

    Format

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+use evalexpr::{Context, DefaultNumericTypes, build_operator_tree, context_map};
 use std::env::args;
-use evalexpr::{DefaultNumericTypes, context_map, build_operator_tree, Context};
 
 fn main() {
     let expr_str = args().nth(1).unwrap();
     let expr = build_operator_tree::<DefaultNumericTypes>(&expr_str).unwrap();
 
-    let mut screen = Screen::new(64, 32);
+    let mut screen = Screen::new(128, 64);
 
     let hsw = (screen.width / 2) as isize;
     let hsh = (screen.height / 2) as isize;
@@ -15,14 +15,19 @@ fn main() {
             let mut ctx = context_map! {
                 "x" => float (x as f64),
                 "y" => float (y as f64),
-            }.unwrap();
+            }
+            .unwrap();
 
             expr.eval_with_context_mut(&mut ctx).unwrap();
 
             let x_val = hsw as f64 + ctx.get_value("x").unwrap().as_float().unwrap();
             let y_val = hsh as f64 - ctx.get_value("y").unwrap().as_float().unwrap();
 
-            if x_val <= 0.0 || x_val >= screen.width as f64 || y_val <= 0.0 || y_val >= screen.height as f64 {
+            if x_val <= 0.0
+                || x_val >= screen.width as f64
+                || y_val <= 0.0
+                || y_val >= screen.height as f64
+            {
                 continue;
             }
 
@@ -34,32 +39,33 @@ fn main() {
 }
 
 struct Screen {
-	width: usize,
-	height: usize,
-	buf: Vec<bool>,
+    width: usize,
+    height: usize,
+    buf: Vec<bool>,
 }
 
 impl Screen {
-	fn new(width: usize, height: usize) -> Screen {
-		let mut s = Screen {
-			width, height,
-			buf: Vec::new(),
-		};
-		
-		for _ in 0..width * height {
-			s.buf.push(false);
-		}
+    fn new(width: usize, height: usize) -> Screen {
+        let mut s = Screen {
+            width,
+            height,
+            buf: Vec::new(),
+        };
 
-		s
-	}
+        for _ in 0..width * height {
+            s.buf.push(false);
+        }
 
-	fn set(&mut self, x: usize, y: usize, on: bool) {
-		self.buf[y * self.width + x] = on;
-	}
+        s
+    }
 
-	fn render(&self) {
-		for y in 0..self.height {
-			for x in 0..self.width {
+    fn set(&mut self, x: usize, y: usize, on: bool) {
+        self.buf[y * self.width + x] = on;
+    }
+
+    fn render(&self) {
+        for y in 0..self.height {
+            for x in 0..self.width {
                 match (x, y) {
                     (x, y) if self.buf[y * self.width + x] => print!("▀"),
                     (x, y) if x == self.width / 2 && y == self.height / 2 => print!("┼"),
@@ -75,8 +81,8 @@ impl Screen {
                     (x, _) if x == self.width / 2 || x == 0 || x == self.width - 1 => print!("│"),
                     _ => print!(" "),
                 }
-			}
-			println!();
-		}
-	}
+            }
+            println!();
+        }
+    }
 }
